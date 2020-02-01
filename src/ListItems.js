4c80e21f0787d540224fd0960595ed21a9b3511f
smishra11 <msubhasish11@gmail.com>
2020-02-02 02:25:15 +0530

    added code for adding todo items to list and delete items from list

--- src/ListItems.js
+++ src/ListItems.js
@@ -5,10 +5,11 @@ import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 function ListItems(props){
   const items = props.items;
   const listItems = items.map(item => {
-    return <div className='list' key='item-key'>
+    return <div className='list' key={item.key}>
       <p>{item.text}
         <span>
-          <FontAwesomeIcon className='faicons' icon='trash'></FontAwesomeIcon>
+          <FontAwesomeIcon className='faicons' icon='trash' 
+          onClick={ () => props.deleteItem(item.key)}/>
         </span>
       </p>
     </div>
