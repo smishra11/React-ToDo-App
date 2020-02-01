323c86405399f4d9f465c5c24f155b433fa91ff9
smishra11 <msubhasish11@gmail.com>
2020-02-02 01:37:13 +0530

    Added delete icon in listitem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import ListItems from './ListItems'
+import ListItems from './ListItems';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
 
 class App extends React.Component{
   constructor(props){
