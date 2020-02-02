f679764e0d6e46b94f10957d27a61efb069cd2fe
smishra11 <msubhasish11@gmail.com>
2020-02-02 17:44:59 +0530

    Added code for updating items from todo list

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component{
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
   handleInput(e){
     this.setState({
@@ -50,6 +51,17 @@ class App extends React.Component{
       items:filteredItems
     })
   }
+  setUpdate(text, key){
+    const items = this.state.items;
+    items.map(item => {
+      if(item.key === key){
+        item.text=text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+  }
   render() {
     return(
       <div className="App">
@@ -57,10 +69,10 @@ class App extends React.Component{
           <form id="to-do-form" onSubmit={this.addItem}>
             <input type="text" placeholder="Enter Text" value={this.state.currentItem.text}
             onChange={this.handleInput}/>
-            <button type="submit">Add</button>
+            <button type="submit">Add</button> 
           </form>
         </header>
-        <ListItems items = {this.state.items} deleteItem ={this.deleteItem}></ListItems>
+        <ListItems items = {this.state.items} deleteItem ={this.deleteItem} setUpdate = {this.setUpdate}></ListItems>
       </div>
     );
   }
