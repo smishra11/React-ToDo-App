f5d78250a6cf4932be5248c6abb5e2cf501415ac
smishra11 <msubhasish11@gmail.com>
2020-02-02 19:39:35 +0530

    Updates

---
+++ precache-manifest.4283ba76f0879eb21a3d8ee35dd9e546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cb0758851a53d325f800e54d72495d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ad6a6b1ee9ae5e2a70a4",
+    "url": "/todolist/static/css/main.6d9d849b.chunk.css"
+  },
+  {
+    "revision": "1f1bb64c5fddfa1bd8f4",
+    "url": "/todolist/static/js/2.f8ba9ac8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.f8ba9ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6a6b1ee9ae5e2a70a4",
+    "url": "/todolist/static/js/main.e0f417a0.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
