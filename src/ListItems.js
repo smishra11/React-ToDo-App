f61654a327f872a6d1360917a8c38547890b0a83
smishra11 <msubhasish11@gmail.com>
2020-02-02 17:43:48 +0530

    Added code for editing items from list

--- src/ListItems.js
+++ src/ListItems.js
@@ -6,7 +6,13 @@ function ListItems(props){
   const items = props.items;
   const listItems = items.map(item => {
     return <div className='list' key={item.key}>
-      <p>{item.text}
+      <p>
+        <input type="text" id={item.key} value={item.text} 
+        onChange={ 
+          (e) => {
+              props.setUpdate(e.target.value, item.key)
+              }
+            }/>
         <span>
           <FontAwesomeIcon className='faicons' icon='trash' 
           onClick={ () => props.deleteItem(item.key)}/>
